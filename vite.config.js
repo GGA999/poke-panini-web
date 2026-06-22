5fbfe350161637ed04abe6493b34915e78efcca2
lbelometti <luca.belometti.0709@gmail.com>
2026-06-22 10:04:12 +0200

    Correct file

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    base: env.VITE_BASE_PATH || '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    historyApiFallback: true,
-  },
-})
+    server: {
+      historyApiFallback: true,
+    },
+    preview: {
+      historyApiFallback: true,
+    },
+  })
+}
